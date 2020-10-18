fd89a65f73d5cf5e5bf96ffee4cfdb6b8119e00f
Paulo <pauloh16.pdr@gmail.com>
2020-10-17 23:31:33 -0300

    Adicionar comentários no arquivo Deletar.js

--- js/modules/Deletar.js
+++ js/modules/Deletar.js
@@ -1,16 +1,25 @@
+// Classe que será exportada.
 export default class Deletar {
-    constructor(form, id) {
-        this.btn = document.querySelector(`${form} #sim`);
+    // Construtor da classe.
+    constructor(seletor, id) {
+        // Seleciona o botão de confimação.
+        this.btn = document.querySelector(`${seletor} #sim`);
+        // Id o prato que será deletado.
         this.id = id;
 
+        // Define a referência do "this" do método deletarDado como sendo a própria classe. (linha 14)
         this.deletarDado = this.deletarDado.bind(this);
     }
 
+    // Método para fazer a requisição ao servidor e deleta os dados no banco de dados.
     deletarDado() {
+        // Puxando o token do navegador.
         const token = sessionStorage.getItem("token");
 
+        // Fetch que faz a requisição ao servidor.
         fetch('http://localhost:3001/pratos', {
             method: "DELETE",
+            // Converte dado com o id em string para enviar ao servidor.
             body: JSON.stringify({ id: this.id }),
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
@@ -19,11 +28,16 @@ export default class Deletar {
         });
     }
 
+    // Método para inicar a classe.
     iniciar() {
+        // Testa se o botão que foi passado existe e se o id foi passado.
         if (this.btn && this.id) {
+            // Caso exista ele adiciona o evento de "click" no botão de confirmação.
+            // O evento executa o método "deletarDado". (linha 15)
             this.btn.addEventListener("click", this.deletarDado);
         }
 
+        // Retorna a própria instância da classe.
         return this;
     }
 }
